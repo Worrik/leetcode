55c679eaf0d3a77c08d727dcf195a5150c1d2f24
Volodymyr <63713148+Worrik@users.noreply.github.com>
2022-10-07 22:53:23 +0300

    Create solution.go

---
+++ 598. Range Addition II/solution.go
@@ -0,0 +1,16 @@
+func maxCount(m int, n int, ops [][]int) int {
+    if len(ops) == 0 {
+        return m * n
+    }
+    minX := ops[0][0]
+    minY := ops[0][1]
+    for _, el := range ops[1:] {
+        if minX > el[0] && el[0] != 0 {
+            minX = el[0]
+        }
+        if minY > el[1] && el[1] != 0 {
+            minY = el[1]
+        }
+    }
+    return minX * minY
+}
